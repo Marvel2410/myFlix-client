02a41cfb9612aa305a022b8ebcc3a9cdaa58ed41
Marvel2410 <139808890+Marvel2410@users.noreply.github.com>
2023-09-29 13:13:08 -0700

    updated movie view component

---
+++ src/movie-view/movie-view.jsx
@@ -0,0 +1,26 @@
+export const MovieView = ({ movie }) => {
+  return (
+    <div>
+      <div>
+        <img src={movie.image} />
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.Title}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.Description}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.director}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.Genre}</span>
+      </div>
+    </div>
+  );
+};
+
