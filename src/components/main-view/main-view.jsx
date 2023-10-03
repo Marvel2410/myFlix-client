9e927e2e9108a187471ea0dd18d5b0b9a14ff00a
Marvel2410 <139808890+Marvel2410@users.noreply.github.com>
2023-10-03 15:00:36 -0700

    updating import statements
    movie folders into components folder had to update paths

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import PropTypes from 'prop-types';
-import "../index.scss";
+import "../../index.scss";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
